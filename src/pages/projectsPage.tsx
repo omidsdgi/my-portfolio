89a3fb073aadc1381b939c5454c21e4369bd5cf3
omidsdgi <omid69sdgi@gmail.com>
2025-08-31 10:51:28 +0330

    implement circular back arrow for home page

--- src/pages/projectsPage.tsx
+++ src/pages/projectsPage.tsx
@@ -6,7 +6,9 @@ import Link from "next/link";
 export default function ProjectsPage() {
     return (
         <main className="max-w-7xl bg-[#555555] mt-5 rounded-3xl mx-auto px-4 py-12">
-            <h1 className="text-5xl text-white font-quicksand font-bold mb-8 dark:text-yellow-300" >Projects</h1>
+            <h1 className="text-5xl text-white font-quicksand font-bold mb-8 dark:text-yellow-300" >
+                <Link href="/" className="inline-flex items-center justify-center w-16 h-16 rounded-full border-2 border-white hover:bg-white hover:text-black transition-all  mr-4">&larr;</Link>
+                Projects</h1>
 
             <div className="grid md:grid-cols-2 gap-12 ">
                 {projects.map((p) => (
