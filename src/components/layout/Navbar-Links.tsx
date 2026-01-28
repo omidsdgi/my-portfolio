7b39a72b1554eb04c00f791eb2af6a7e2b434f70
omidsdgi <omid69sdgi@gmail.com>
2026-01-28 22:10:03 +0330

    feat: implement Navbar-Links

---
+++ src/components/layout/Navbar-Links.tsx
@@ -0,0 +1,26 @@
+import {links} from "@/Data";
+import Link from "next/link";
+
+const NavbarLinks = () => {
+    return (
+        <nav   className="bg-emerald-100 text-gray-900" >
+            <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:flex-wrap gap-x-16 sm:items-center sm:py8"
+            >
+                <h2 className='text-3xl font-bold'>Web<span className='text-emerald-600 '>Dev</span></h2>
+                <div className='flex gap-x-3'>
+                  {links.map((item ) => {
+                        const{id, href, text} = item;
+                        return (
+                            <Link href={href} key={id}
+                                  className='capitalize text-lg tracking-wide hover:text-emerald-500 duration-300 '>
+                                {text}
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavbarLinks;
