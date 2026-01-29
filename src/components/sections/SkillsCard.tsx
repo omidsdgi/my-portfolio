c2ef04cf581be3d5017eb58f679efe01c6eb4f31
omidsdgi <omid69sdgi@gmail.com>
2026-01-29 18:29:06 +0330

    feat: implement SkillsCard component

--- src/components/sections/SkillsCard.tsx
+++ src/components/sections/SkillsCard.tsx
@@ -6,7 +6,7 @@ const SkillsCard = ({icon, title, text}:Skill) => {
     return (
         <article  className="flex flex-col h-full  p-4  shadow-sm hover:shadow-md transition-all">
             <span>{icon}</span>
-            <h4>{title}</h4>
+            <h4 className='mt-4 mb-6 font-bold'>{title}</h4>
             <p>{text[lang]}</p>
         </article>
     );
