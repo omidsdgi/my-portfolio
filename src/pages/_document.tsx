fb23ec5d46afd5564c4ce1e5c104fbdb00b93bb4
omidsdgi <omid69sdgi@gmail.com>
2026-01-31 09:42:38 +0330

    Add base classes for background, smooth scrolling, and dark mode to HTML

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-      <Html lang="en" className="light">
+      <Html lang="en" className="bg-slate-50 scroll-smooth dark">
           <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
           </Head>
