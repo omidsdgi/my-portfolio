fc33daa3421cadeca46fa6402222fdaea58c7256
omidsdgi <omid69sdgi@gmail.com>
2026-02-01 12:54:07 +0330

    feat: implement initial Navbar-Links.tsx & SectionTitle.tsx components

--- src/components/sections/SectionTitle.tsx
+++
@@ -1,9 +0,0 @@
-const SectionTitle = ({text}:{text:string}) => {
-    return (
-        <div className='border-b border-gray-200 pb-5'>
-            <h2 className='text-3xl font-medium tracking-wider capitalize'>{text}</h2>
-        </div>
-    );
-};
-
-export default SectionTitle;
