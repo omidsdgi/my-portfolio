d25a2ab316f49efdea1b9829d47cf59bb30735e7
omidsdgi <omid69sdgi@gmail.com>
2026-02-04 08:13:20 +0330

    fix: resolve npm install issues by configuring Iranian registry

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  images: {
-    domains: ["github.com", "user-attachments.githubusercontent.com"],
-  },
+    /* config options here */
+    reactStrictMode: true,
+    images: {
+        domains: ["github.com", "user-attachments.githubusercontent.com"],
+    },
 };
 
 export default nextConfig;
