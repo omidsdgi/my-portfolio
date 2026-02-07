f85161eb514c566cae028e089b48affa7099482a
omidsdgi <omid69sdgi@gmail.com>
2026-02-07 20:47:27 +0330

    chore(next): remove remote image config due to using local images

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactStrictMode: true,
-    images: {
-        domains: ["github.com", "user-attachments.githubusercontent.com"],
-    },
+
 };
 
 export default nextConfig;
