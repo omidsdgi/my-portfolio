824de7098c7f51a6f786d9cade6a1be81a8f0e80
omidsdgi <omid69sdgi@gmail.com>
2026-02-16 23:09:08 +0330

    feat(navLinks): apply DaisyUI theme colors and hover background

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { links } from "@/data/Data";
+import React from "react";
 
 type NavLinksProps = {
     onSmoothScroll?: (e: React.MouseEvent<HTMLAnchorElement>, href: string) => void;
@@ -14,7 +15,7 @@ const NavLinks = ({onSmoothScroll}:NavLinksProps) => {
                     <Link
                         href={item.href}
                         onClick={(e) => onSmoothScroll?.(e as React.MouseEvent<HTMLAnchorElement>, item.href)}
-                        className="group capitalize text-sm md:text-base tracking-wide relative px-3 py-2 transition-colors duration-300 hover:bg-slate-400 rounded-md"
+                        className=" text-base-content font-medium px-5 py-3 rounded-md transition-colors duration-300 hover:bg-primary hover:bg-opacity-20     hover:text-primary"
                     >
                         {item.text}
                     </Link>
